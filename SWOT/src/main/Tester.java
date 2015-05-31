625a9d36cb514431083bb690fb1f7d63f1f9245c
SBEntreprises <chillindude.911@gmail.com>
2015-05-31 02:11:46 -0500

    More Dumb Tester Stuff

--- SWOT/src/main/Tester.java
+++ SWOT/src/main/Tester.java
@@ -9,13 +9,15 @@ public class Tester {
 		Idea ideaOne = new Idea("S","Alex and I did It",2);
 		Idea ideaTwo = new Idea("W","GUI is not working with it all",6);
 		Idea ideaThree = new Idea("O","We can use this in the future",8);
-		Idea ideaFour = new Idea("T","I'll forget how this code works",10);
+		Idea ideaFour = new Idea("T","I'll forget how this code works",6);
+		Idea ideaFive = new Idea("T", "Your mom's a bitch",3);
 		ArrayList<Idea> ideaList = new ArrayList<Idea>();
 		
 		ideaList.add(ideaOne);
 		ideaList.add(ideaTwo);
 		ideaList.add(ideaThree);
 		ideaList.add(ideaFour);
+		ideaList.add(ideaFive);
 		
 		Logic testerLogic = new Logic(ideaList);
 		
