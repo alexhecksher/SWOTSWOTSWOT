ca136054eb1673c310370bb71743a44707d6236f
SBEntreprises <chillindude.911@gmail.com>
2015-05-31 00:50:05 -0500

    Buffed up Logic Class
    added private compute(), getCompositeScore(), getImmediateScore(), and
    getFutureScore()

--- SWOT/src/main/Logic.java
+++ SWOT/src/main/Logic.java
@@ -5,8 +5,42 @@ import java.util.ArrayList;
 public class Logic {
 	
 	private ArrayList<Idea> logIdea;
+	int immediate;
+	int future;
+	int composite;
 	
+	//establish Logic given parameter of ArrayList of Ideas
 	public Logic(ArrayList<Idea> IdeaArrayList) {
 		logIdea = IdeaArrayList;
+		compute();
 	}
+	
+	//Compute total scores for each idea
+	private void compute() {
+		int sTotal=0, wTotal=0, oTotal=0, tTotal=0;
+		for (Idea ideaTemp : logIdea) {
+			if (ideaTemp.SWOTc=="S")
+				sTotal+=ideaTemp.value;
+			if (ideaTemp.SWOTc=="W")
+				wTotal+=ideaTemp.value;
+			if (ideaTemp.SWOTc=="O")
+				oTotal+=ideaTemp.value;
+			if (ideaTemp.SWOTc=="T")
+				tTotal+=ideaTemp.value;
+		}
+		immediate = sTotal-wTotal;
+		future = oTotal-tTotal;
+	}
+	
+	//gets()
+	public int getCompositeScore() {
+		return composite;
+	}
+	public int getImmediateScore() {
+		return immediate;
+	}
+	public int getFutureScore() {
+		return future;
+	}
+	
 }
